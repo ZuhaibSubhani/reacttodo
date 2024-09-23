5134bca8dcf43a4544bcb6cf00f37c2ee3d49a09
divyanshupal1 <divyanshupal77@gmail.com>
2024-09-23 12:20:54 +0530

    updates:frontend

--- frontend/src/component/CreateTodo.jsx
+++ frontend/src/component/CreateTodo.jsx
@@ -1,11 +1,44 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+import { axiosInstance } from "../lib/axiosInstance"
 
 
-export function CreateTodo(){
+export function CreateTodo({fetchTodos}){
+
+    const [todo,setTodo] = useState({
+        title:"",
+        description:"",
+        completed:false
+    })
+    async function createTodo(){
+        const res = await axiosInstance.post('/todo',todo)
+        console.log(res.data)
+        fetchTodos()
+    }
+    
     return(
         <div>
-        <input type="text" placeholder="title" /> <br />
-        <input type="text" placeholder="description"/><br />
-        <button>+</button>
-</div>
+            <input 
+                type="text" 
+                placeholder="title" 
+                value={todo.title}
+                onChange={(e)=>setTodo({
+                    ...todo,
+                    title:e.target.value
+                })}
+            /> <br />
+
+            <input 
+                type="text" 
+                placeholder="description"
+                value={todo.description}
+                onChange={(e)=>setTodo({
+                    ...todo,
+                    description:e.target.value
+                })}
+            /><br />
+
+            <button onClick={createTodo}>+</button>
+        </div>
     )
 }   
--- frontend/src/component/Todos.jsx
+++ frontend/src/component/Todos.jsx
@@ -1,7 +1,33 @@
-export function Todos({todos}){
+/* eslint-disable react/prop-types */
+import { axiosInstance } from "../lib/axiosInstance"
+
+
+export function Todos({todos,fetchTodos}){
+    console.log("Todo list")
+    console.log(todos)
+
+    async function markCompleted(id){
+        const res = await axiosInstance.put(`/completed`,{
+            id:id
+        })
+        console.log(res.data)
+        fetchTodos()
+    }
+
 return (
     <div>
-
+        {
+            todos?.map((todo)=>{
+                return (
+                    <div key={todo._id} style={{display:"flex",columnGap:"10px"}}>
+                        <h4>{todo.title}</h4>
+                        <p>{todo.description}</p>
+                        <p>{todo.completed?"completed":"not completed"}</p>
+                        <button onClick={()=>markCompleted(todo._id)}>Mark Completed</button>
+                    </div>
+                )
+            })
+        }
     </div>
 )
 }
